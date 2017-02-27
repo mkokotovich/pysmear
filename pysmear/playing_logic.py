d575088c238a1653b96775e09305cd731ee71f9c
Matthew Kokotovich <mkokotovich@gmail.com>
2017-02-27 12:01:06 -0600

    Add the test, outline some playing logic

--- pysmear/playing_logic.py
+++ pysmear/playing_logic.py
@@ -87,3 +87,89 @@ class JustGreedyEnough(SmearPlayingLogic):
                 idx = self.find_lowest_card_index(my_hand, current_hand.current_trick.lead_suit, current_hand.trump)
 
         return idx
+
+
+
+# TODO
+class CautiousTaker(SmearPlayingLogic):
+    def find_lowest_card_index_to_beat(self, my_hand, card_to_beat, lead_suit, trump):
+        lowest_index = None
+        if utils.is_trump(card_to_beat, trump):
+            # Card to beat is trump, see if I have a higher trump
+            my_trump = utils.get_trump_indices(trump, my_hand)
+            for idx in my_trump:
+                if utils.is_less_than(card_to_beat, my_hand[idx], trump):
+                    lowest_index = idx
+        elif card_to_beat.suit == lead_suit:
+            my_trump = utils.get_trump_indices(trump, my_hand)
+            if len(my_trump) != 0:
+                # Card to beat isn't trump, but I have trump. Play my lowest trump
+                lowest_index = my_trump[0]
+            else:
+                matching_idxs = my_hand.find(card_to_beat.suit, sort=True, ranks=rank_values)
+                for idx in matching_idxs:
+                    # Play the lowest card in the matching suit that will beat the card to beat
+                    if my_hand[idx].gt(card_to_beat, ranks=rank_values):
+                        lowest_index = idx
+        else:
+            # Card to beat isn't trump and isn't the lead suit, this doesn't make sense
+            print "Error: card to beat seems incorrect: {}".format(card_to_beat)
+        return lowest_index
+        
+    def find_strongest_card(self, my_hand, trump):
+        my_trump = utils.get_trump_indices(trump, my_hand)
+        strongest_idx = None
+        if len(my_trump) != 0:
+            strongest_idx = my_trump[-1]
+        else:
+            my_hand.sort(ranks=rank_values)
+            strongest_idx = len(my_hand) - 1
+        return strongest_idx
+
+    def find_lowest_card_index(self, my_hand, lead_suit, trump):
+        lowest_index = None
+        lowest_trump_index = None
+        # First try following suit.
+        my_hand.sort(ranks=rank_values)
+        indices = []
+        if lead_suit == "Trump":
+            indices = utils.get_trump_indices(trump, my_hand)
+            indices.reverse()
+        else:
+            indices = my_hand.find(lead_suit, sort=True, ranks=rank_values)
+        if len(indices) == 0:
+            indices = range(0, len(my_hand))
+        for idx in indices:
+            if utils.is_trump(my_hand[idx], trump) and lowest_trump_index == None:
+                lowest_trump_index = idx
+            else:
+                lowest_index = idx
+                break
+        if lowest_index == None:
+            lowest_index = lowest_trump_index
+        return lowest_index
+
+    def choose_card(self, current_hand, card_counting_info, my_hand):
+        idx = 0
+        if len(current_hand.current_trick.cards) == 0:
+            # First player, leading the trick...
+            # Play A, K, Q of trump
+            # Play A, K, Q, J of other suits
+            # Play low of other suit
+            # Play lowest trump
+            idx = self.find_strongest_card(my_hand, current_hand.trump)
+        else:
+            # Not the first player
+            # If I can take a Jack or Jick, take it
+            # If I can take a 10, take it
+            # If there are high trump still out but I can safely take home my jack or jick, play it
+            # If I can take the trick with a non-trump, take it
+            # If there is a face card and I have two or more low trump, take it
+            # Play a loser
+            # Play lowest trump
+            idx = self.find_lowest_card_index_to_beat(my_hand, current_hand.current_trick.current_winning_card, current_hand.current_trick.lead_suit, current_hand.trump)
+            if idx == None:
+                # If we can't beat it, then just play the lowest card, following suit as needed
+                idx = self.find_lowest_card_index(my_hand, current_hand.current_trick.lead_suit, current_hand.trump)
+
+        return idx
---
+++ tests/test_card_counting.py
@@ -0,0 +1,114 @@
+import unittest
+from mock import MagicMock
+import sys
+
+sys.path.insert(0, "..")
+from pysmear.smear_utils import SmearUtils as utils
+from pysmear.card_counting import CardCounting
+import pydealer
+
+
+class TestSafeToPlay(unittest.TestCase):
+    def setUp(self):
+        self.cc = CardCounting(num_players = 4)
+        self.trump = "Spades"
+        self.current_hand = MagicMock()
+        self.current_hand.current_trick = MagicMock()
+        self.current_hand.trump = self.trump
+        self.current_hand.current_trick.lead_suit = "Trump"
+
+        self.jack_hearts = pydealer.Card(value="Jack", suit="Hearts")
+        self.jack_spades = pydealer.Card(value="Jack", suit="Spades")
+        self.jack_diamonds = pydealer.Card(value="Jack", suit="Diamonds")
+        self.jack_clubs = pydealer.Card(value="Jack", suit="Clubs")
+
+        self.ace_spades = pydealer.Card("Ace", "Spades")
+        self.king_spades = pydealer.Card("King", "Spades")
+        self.queen_spades = pydealer.Card("Queen", "Spades")
+        self.ten_spades = pydealer.Card("10", "Spades")
+
+        self.two_diamonds = pydealer.Card("2", "Diamonds")
+        self.queen_hearts = pydealer.Card("Queen", "Hearts")
+        self.seven_clubs = pydealer.Card("7", "Clubs")
+        self.two_spades = pydealer.Card("2", "Spades")
+        self.cards = [self.ace_spades, self.two_diamonds, self.queen_hearts,
+            self.seven_clubs, self.jack_clubs, self.two_spades]
+
+        self.current_hand.current_trick.cards = self.cards
+
+
+    def test_jick_is_safe_after_a_k_q_j(self):
+        self.cc.cards_played["Trump"].append(self.ace_spades)
+        self.cc.cards_played["Trump"].append(self.king_spades)
+        self.cc.cards_played["Trump"].append(self.queen_spades)
+        self.cc.cards_played["Trump"].append(self.jack_spades)
+
+        self.current_hand.current_trick.cards = [ self.two_diamonds ]
+        safe = self.cc.safe_to_play(0, self.jack_clubs, self.current_hand)
+        self.assertEqual(safe, True)
+
+
+    def test_two_is_safe_after_everyone_is_out_of_trump(self):
+        self.cc.card_was_played(0, self.ace_spades, self.current_hand)
+        self.cc.card_was_played(1, self.two_diamonds, self.current_hand)
+        self.cc.card_was_played(2, self.queen_hearts, self.current_hand)
+        self.cc.card_was_played(3, self.seven_clubs, self.current_hand)
+
+        self.current_hand.current_trick.cards = []
+        safe = self.cc.safe_to_play(0, self.two_spades, self.current_hand)
+        self.assertEqual(safe, True)
+
+    def test_jick_is_safe_if_only_player_behind_me_is_out(self):
+        self.cc.player_out_of_cards[1]["Trump"] = True
+        self.current_hand.current_trick.cards = [ self.two_diamonds, self.seven_clubs ]
+        safe = self.cc.safe_to_play(0, self.jack_clubs, self.current_hand)
+        self.assertEqual(safe, True)
+
+
+class TestHighestCardStillOut(unittest.TestCase):
+    def setUp(self):
+        self.cc = CardCounting(num_players = 4)
+        self.trump = "Spades"
+        self.current_hand = MagicMock()
+        self.current_hand.current_trick = MagicMock()
+        self.current_hand.trump = self.trump
+        self.current_hand.current_trick.lead_suit = self.trump
+
+        self.jack_hearts = pydealer.Card(value="Jack", suit="Hearts")
+        self.jack_spades = pydealer.Card(value="Jack", suit="Spades")
+        self.jack_diamonds = pydealer.Card(value="Jack", suit="Diamonds")
+        self.jack_clubs = pydealer.Card(value="Jack", suit="Clubs")
+
+        self.ace_spades = pydealer.Card("Ace", "Spades")
+        self.king_spades = pydealer.Card("King", "Spades")
+        self.queen_spades = pydealer.Card("Queen", "Spades")
+        self.ten_spades = pydealer.Card("10", "Spades")
+
+        self.two_diamonds = pydealer.Card("2", "Diamonds")
+        self.queen_hearts = pydealer.Card("Queen", "Hearts")
+        self.seven_clubs = pydealer.Card("7", "Clubs")
+        self.two_spades = pydealer.Card("2", "Spades")
+        self.cards = [self.ace_spades, self.two_diamonds, self.queen_hearts,
+            self.seven_clubs, self.jack_clubs, self.two_spades]
+
+        self.current_hand.current_trick.cards = self.cards
+
+
+    def test_jick_is_highest_after_a_k_q_j(self):
+        self.cc.cards_played["Trump"].append(self.ace_spades)
+        self.cc.cards_played["Trump"].append(self.king_spades)
+        self.cc.cards_played["Trump"].append(self.queen_spades)
+        self.cc.cards_played["Trump"].append(self.jack_spades)
+
+        card = self.cc.highest_card_still_out("Spades", True)
+        self.assertEqual(card, self.jack_clubs)
+
+    def test_10_is_highest_after_a_k_q_j_j(self):
+        self.cc.cards_played["Trump"].append(self.ace_spades)
+        self.cc.cards_played["Trump"].append(self.king_spades)
+        self.cc.cards_played["Trump"].append(self.queen_spades)
+        self.cc.cards_played["Trump"].append(self.jack_spades)
+        self.cc.cards_played["Trump"].append(self.jack_clubs)
+
+        card = self.cc.highest_card_still_out("Spades", True)
+        self.assertEqual(card, self.ten_spades)
